63c3b18488ccbbfe1639c1706967edecbaad6242
MiaFate <miapurplefroot@gmail.com>
2023-04-06 02:16:49 -0300

    p-5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   const verbos = generateIrregularVerbs();
   return (
     <main >
-      <div className="flex flex-col md:grid md:grid-cols-4 gap-4">
+      <div className="flex flex-col md:grid md:grid-cols-4 gap-4 p-5">
         {verbos && verbos.map((verb) => (
           <Card key={verb.infinitive} infinitive={verb.infinitive} pastSimple={verb.pastSimple} pastParticiple={verb.pastParticiple} />
         ))}
