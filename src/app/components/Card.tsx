331ffa1baec1bb2f7f9cd2e1eed1fd9dd7253a14
MiaFate <miapurplefroot@gmail.com>
2023-04-06 04:17:53 -0300

    distribucion de las palabras

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -30,14 +30,14 @@ const CardContent: React.FC<CardContentProps> = ({ face, infinitive, pastSimple,
     if (face === "front") {
         return (
             <>
-                <div className="flex flex-row justify-center p-4 mb-1 bg-zinc-800 rounded-md shadow-lg">
+                <div className="flex flex-row place-content-center p-4 mb-1 bg-zinc-800 rounded-md shadow-lg">
                     <h2 className="cards-title text-yellow-50">{infinitive}</h2>
                 </div>
             </>)
     }
     return (
         <>
-            <div className="flex flex-row justify-around p-4 mb-1 bg-slate-600 align-middle rounded-md shadow-lg">
+            <div className="flex flex-row place-content-evenly p-4 mb-1 bg-slate-600  rounded-md shadow-lg">
                 <h2 className="cards-title text-yellow-50">{infinitive}</h2>
                 <h2 className="cards-title text-yellow-50">{pastSimple}</h2>
                 <h2 className="cards-title text-yellow-50">{pastParticiple}</h2>
