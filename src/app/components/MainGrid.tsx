0309a6a9dc5776a4724b96b209b7a3846bb248e7
MiaFate <miapurplefroot@gmail.com>
2023-04-07 04:55:28 -0300

    refactorizacion de componentes

---
+++ src/app/components/MainGrid.tsx
@@ -0,0 +1,18 @@
+
+import { generateIrregularVerbs } from '../api/irregular-verbs/route';
+import { Card } from './Card';
+
+//export const dynamic = 60; esto genera una pagina cada 60 seg cuando un cliente accede a ella
+//export const revalidate = Infinity;
+export default function MainGrid() {
+
+  const verbos = generateIrregularVerbs();
+  return (
+    <div className="flex flex-col md:grid md:grid-cols-3 gap-4 p-5">
+      {verbos && verbos.map((verb) => (
+        <Card key={verb.infinitive} verb={verb} />
+      ))}
+    </div>
+  )
+}
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,10 @@
+import MainGrid from "./components/MainGrid";
 
-import { generateIrregularVerbs } from './api/irregular-verbs/route';
-import { Card } from './components/Card';
-
-//export const dynamic = 60; esto genera una pagina cada 60 seg cuando un cliente accede a ella
-//export const revalidate = Infinity;
 export default function Home() {
 
-  const verbos = generateIrregularVerbs();
-  return (
-    <main >
-      <div className="flex flex-col md:grid md:grid-cols-3 gap-4 p-5">
-        {verbos && verbos.map((verb) => (
-          <Card key={verb.infinitive} verb={verb} />
-        ))}
-      </div>
-    </main>
-  )
+    return (
+        <main >
+            <MainGrid />
+        </main>
+    )
 }
-
