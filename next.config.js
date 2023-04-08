b0d717357378a92fbe839d0867f01ef6c4eb093e
MiaFate <miapurplefroot@gmail.com>
2023-04-07 21:42:31 -0300

    pwa test

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public'
+});
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
 }
 
-module.exports = nextConfig
+module.exports = withPWA({
+  experimental: {
+    appDir: true,
+  },
+}) 
