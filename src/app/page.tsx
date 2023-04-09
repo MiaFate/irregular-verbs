3c5cd3169340edcb374ad576f19bb1da4c3b3301
MiaFate <miapurplefroot@gmail.com>
2023-04-09 06:20:23 -0300

    added comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ import { Footer } from './components/Footer'
 
 const Home = () => {
     return (
-        <main className='max-h-full flex flex-col'>
+        <main className='max-h-full flex flex-col'> {/*limita el alto de la pagina al tamaño del body, el cual tiene un height de 100vh y margin 0 */}
             <header >
                 <NavBar />
             </header>
-            <div className="grow overflow-auto">
+            <div className="grow overflow-auto">{/*contenedor principal el cual tomaralo que sobre del header y footer y generara scroll si tiene overflow */}
                 <MainGrid />
             </div>
             <footer>
