02d34ae41797ce67da7d0f26eed8c9df942a246a
MiaFate <miapurplefroot@gmail.com>
2023-04-10 08:25:06 -0300

    added game

--- src/app/components/MainGrid.tsx
+++ src/app/components/MainGrid.tsx
@@ -27,7 +27,7 @@ export const MainGrid = () => {
         :
         <div className='h-[120px] md:col-start-2'>
           <div className="flex flex-row h-full place-content-center place-items-center p-4 mb-1 bg-zinc-800  dark:bg-zinc-900 rounded-md shadow-lg select-none">
-            <h2 className="cards-title font-bold text-yellow-50 dark:text-gray-300 capitalize">No verbs match</h2>
+            <h2 className="cards-title font-bold text-yellow-50 dark:text-gray-300 capitalize">Verb not found</h2>
           </div>
         </div>
       }
